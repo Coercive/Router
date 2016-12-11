85f5ea25e97897d88992ab4d1e7ce3aece23129a
Anthony <anthony@aouka.com>
2016-12-11 18:01:08 +0100

    Version

--- Router.php
+++ Router.php
@@ -12,7 +12,7 @@ use \Exception;
  * (FR) La simplicité est la sophistication suprême.
  * Léonard de Vinci
  *
- * @version		2.0.0
+ * @version		2.0.1
  * @package		Coercive\Utility\Router
  * @link		@link https://github.com/Coercive/Router
  *
