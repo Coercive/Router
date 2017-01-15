3228caf5b8e5ebd9f706b7ce30b7b161267972cc
Anthony <anthony@aouka.com>
2017-01-15 21:37:23 +0100

    Exceptions files

---
+++ lib/Exception/RouterException.php
@@ -0,0 +1,16 @@
+<?php
+namespace Coercive\Utility\Router\Exception;
+
+use Exception;
+
+/**
+ * RouterException
+ *
+ * @package		Coercive\Utility\Router
+ * @link		@link https://github.com/Coercive/Router
+ *
+ * @author  	Anthony Moral <contact@coercive.fr>
+ * @copyright   (c) 2016 - 2017 Anthony Moral
+ * @license 	http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+class RouterException extends Exception {}
